637f3af65a8ca666ec25b3340ca166611f029bee
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-05-10 23:37:24 +0200

    les emails fonctionnent!

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,8 +53,25 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .about_shadow, .skills__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200});
-sr.reveal('.work__card, .contact__input',{interval: 200});
+sr.reveal('.work__card, .contact__input, .contact__form label',{interval: 200});
 sr.reveal('.skills__data',{interval: 100});
 
 // projects
 sr.reveal('.banner__text, .section-title, .project__container, #youtube_icon',{ interval: 200});
+
+// contact form
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('service_y894zb9', 'template_f6xsmu7', this, 'tCrSFgEm6pbHr-AOm')
+          .then(function() {
+              console.log('SUCCESS!');
+          }, function(error) {
+              console.log('FAILED...', error);
+          });
+  });
+}
