556bd9460f0f59f1cf228f37f52560eafd851992
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-05-12 17:18:46 +0200

    modif shadow, bttn send, + connexion js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,9 @@ const sr = ScrollReveal({
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
-sr.reveal('.home__img, .about__subtitle, .about__text, .about_shadow, .skills__img',{delay: 400});
-sr.reveal('.home__social-icon, .bx-rocket, .;qproject__social-icon',{ interval: 200});
-sr.reveal('.work__card, .contact__input, .contact__form label',{interval: 200});
+sr.reveal('.home__img, .about_shadow, .about__subtitle, .about__text, .about_shadow, .contact_text',{delay: 400});
+sr.reveal('.home__social-icon, .bx-rocket, .project__social-icon',{ interval: 200});
+sr.reveal('.work__card, .contact__input, .contact__form label, .button',{interval: 200});
 sr.reveal('.skills__data',{interval: 100});
 
 // projects
